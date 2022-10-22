f126e72e1bfc9a4212d271fd3ea8c6c06c552e0d
Nord3D <92642439+Nord3D@users.noreply.github.com>
2022-10-23 01:31:09 +0400

    Update snippets.c

--- snippets.c
+++ snippets.c
@@ -1,9 +1,87 @@
 
-function vector dirproject(
-                        vector a; /// vector to project
-                        vector b; /// vector to project onto, normalized
-                        vector d; /// projection direction, normalized
-                            )
+/*** MATH CONSTANTS ***/
+#define NM_PI           3.14159265358979323846
+#define NM_1_3          0.33333333333333333333      /// 1/3
+#define NM_2_3          0.66666666666666666667      /// 2/3
+#define NM_SQRT3_4      0.43301270189221932338      /// sqrt(3) / 4
+#define NM_3SQRT3_4     1.29903810567665797014      /// 3 * sqrt(3) / 4     <Area of a Unit Equilateral Triangle>
+#define NM_SQRT2_12     0.11785113019775792073      /// sqrt(2) / 12        <Volume of a Regular Tetrahedron with Unit edge length>
+
+
+vector2 rot90(vector2 v)
+{
+    vector2 r = v;
+    r[0] = v[1];
+    r[1] = -v[0];
+    return r;
+}
+
+int linesintersection(vector2 p11, p12, p21, p22, p_inter)
+    /// Compute two lines intersection point in 2D
+    /// p11, p12 - first line; p21, p22 - second line points; p_inter - resultant intersection point
+    /// From https://www.topcoder.com/community/competitive-programming/tutorials/geometry-concepts-line-intersection-and-its-applications/
+{
+    float A1 = p12.y - p11.y;
+    float B1 = p11.x - p12.x;
+    float C1 = A1 * p11.x + B1 * p11.y;
+    
+    float A2 = p22.y - p21.y;
+    float B2 = p21.x - p22.x;
+    float C2 = A2 * p21.x + B2 * p21.y;
+    
+    float det = A1 * B2 - A2 * B1;
+    if (det != 0){
+        p_inter.x = (B2 * C1  -  B1 * C2) / det;
+        p_inter.y = (A1 * C2  -  A2 * C1) / det;
+        return 1;   /// Intersection point exist
+    }else{
+        return -1;  /// Lines are parallel
+    }
+}
+
+float skewlinesdist(vector P11, P12, P21, P22, C1, C2)
+/// P1X, P2X - lines points
+/// C1, C2 - nearest points on the 1- and 2- lines
+/// From https://en.wikipedia.org/wiki/Skew_lines#Nearest_points
+{
+    vector D1 = normalize(P12 - P11);
+    vector D2 = normalize(P22 - P21);
+    
+    vector N = cross(D1, D2);
+    vector N1 = cross(D1, N);
+    vector N2 = cross(D2, N);
+    
+    vector S = P21 - P11;
+    float d1 = dot( S, N2) / dot(D1, N2);
+    float d2 = dot(-S, N1) / dot(D2, N1);
+    
+    /*** R E T U R N ***/
+    C1 = P11 + d1 * D1;
+    C2 = P21 + d2 * D2;
+    return distance(C1, C2);
+}
+
+int rayplaneintersection(vector r, r0, pn, p0; float d)
+    /// r - ray (unit vector), r0 - ray starting point, pn - plane normal, p0 - plane point 
+    /// d - resultant ray distance from starting point to intersection point with plane
+    /// From https://en.wikipedia.org/wiki/Line%E2%80%93plane_intersection#Algebraic_form
+{
+    float denominator = dot(r, pn);
+    if (denominator == 0){
+        return 0;                               /// parallel case
+    }else{
+        d = dot(p0 - r0, pn) / denominator;  
+        return 1;                               /// Intersection point exist
+    }
+}
+
+
+
+vector dirproject(
+                  vector a; /// vector to project
+                  vector b; /// vector to project onto, normalized
+                  vector d; /// projection direction, normalized
+                  )
 /*https://math.stackexchange.com/questions/3027898/projection-of-vector-into-an-axis-along-a-direction*/
 {
     /* Remove from a it's projection onto d */
